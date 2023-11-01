7f4128a2e9d50b84c4d3f5111adffa5fb945f801
Ghabz <alghabidarajani02@gmail.com>
2023-11-01 22:35:24 +0800

    Create delete.php

---
+++ functions/delete.php
@@ -0,0 +1,9 @@
+<?php
+    // including the database connection file
+    include_once("../dbConnection/mysqlconfig_connection.php");
+    //getting id of the data from url
+    $id = $__GET['id'];
+    //deleting the row from table
+    mysqli_query($dbc, "DELETE FROM tblsubject WHERE subject_id='$id'");
+    //redirecting to the display page
+    header("Location: ../index.php")
