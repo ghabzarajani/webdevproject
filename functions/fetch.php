7023cc144cdfc6d9e3a1d9890e83dbfba2cb0181
Ghabz <alghabidarajani02@gmail.com>
2023-11-28 21:18:57 +0800

    Update fetch.php

--- functions/fetch.php
+++ functions/fetch.php
@@ -1,5 +1,5 @@
 <?php
     //fetching data
-    $query = "SELECT * FROM tblsubject";
+    $query = "SELECT * FROM tblsubjects LEFT JOIN tblsyllabus ON tblsyllabus.subject_id = tblsubjects.subjectid;";
     $result = msqli_query($dbc, $query); //using mysqli_query
 ?>
